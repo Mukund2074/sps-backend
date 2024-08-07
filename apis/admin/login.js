50416db033a630d894c3fdc3711b0610284e0a6b
mukund hadiya <mukundh.astuteinfosolution@gmail.com>
2024-08-07 16:36:48 +0530

    Fixed bug and updated the code

--- apis/admin/login.js
+++ apis/admin/login.js
@@ -1,46 +1,23 @@
-const connectDB = require('../../db/dbconnect');
+const connectDB = require('../../db/dbConnect');
 
-async function AdminLoginApi(req, res) 
-{
-
-    try 
-    {
+async function AdminLoginApi(req, res) {
+    try {
         const db = await connectDB();
         const collection = db.collection('admindata');
-        const {email , password} = req.body;  
-        const user = await collection.findOne( {email, password} );
+        const { email, password } = req.body;
+        const user = await collection.findOne({ email, password });
 
-        
-
-        if (!user) 
-        {
+        if (!user) {
             return res.status(401).json({ success: false, message: "Invalid username or password" });
-          
-
-            // if (userExist)
-            // {
-            //     return res.status(200).json({ message: 'user logged in successfully...!' });
-            // }
-
-            // return res.status(400).json({ message: 'User does not exist!' });
-         
-        }
-        else{
-            req.session.user = {session: user ,isAuth:true }
-
+        } else {
+            req.session.user = { session: user, isAuth: true };
             const userDatas = req.session.user;
-
-            res.status(200).json({userData : userDatas , success: true, message:"LOGIN SUCCESSFULLY"})
+            res.status(200).json({ userData: userDatas, success: true, message: "LOGIN SUCCESSFULLY" });
         }
-
-        // else{
-        //     return res.status(400).json({ message: 'please fill required fields!' });
-           
-        // }
-    }
-    catch (err) {
+    } catch (err) {
         res.status(500).json({ success: false, error: "Login Failed" });
     }
 }
 
 module.exports = { AdminLoginApi };
+ 
